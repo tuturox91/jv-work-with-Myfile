3c7f1c877f7cd320988d43a009606bad24a6c3ae
John Doe <tuturox91@gmail.com>
2022-10-05 20:05:09 +0300

    Finish

---
+++ src/main/java/core/basesyntax/Pair.java
@@ -0,0 +1,43 @@
+package core.basesyntax;
+
+import javax.lang.model.element.Name;
+import java.util.Comparator;
+
+public class Pair implements Comparable {
+    private String _name;
+    private int _ammount;
+
+    public Pair(String name, int ammount) {
+        this._name = name;
+        this._ammount = ammount;
+    }
+
+    public boolean equalByName(String other) {
+        return this._name.equals(other);
+    }
+
+    public void addAmmount(int ammount) {
+        _ammount+= ammount;
+    }
+
+    public String get_name() {
+        return _name;
+    }
+
+    public int get_ammount() {
+        return _ammount;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder("Name: ").append(_name).append(" Ammount: ").append(_ammount).toString();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if(this.getClass().equals(o.getClass())) {
+            return ((Pair) o).get_ammount() - _ammount;
+        }
+        return 0;
+    }
+}
